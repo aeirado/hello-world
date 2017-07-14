673fbf39fcebf88037fab379154e5991e2739a64
andre <aeirado@users.noreply.github.com>
2017-07-14 18:37:49 -0300

    esse cimmit é do vscode - lp3thw

---
+++ LP3THW/ex16.py
@@ -0,0 +1,22 @@
+from sys import argv
+
+script, filename = argv
+
+print(f"We're going to erase {filename}.")
+print("If you don't want that, hit CTRL-C (^C).")
+print("If you do want that, hit RETURN.)
+
+input("?")
+
+print("Opening the file...")
+target = open(filename, 'w')
+
+print("Truncating the file. Goodbye!")
+target.truncate()
+
+print("Now I'm going to ask you for three lines.")
+
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
+
