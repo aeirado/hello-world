4d2edb0aa57ad03a5356225114464dee86ac6f45
andre <aeirado@users.noreply.github.com>
2017-07-22 15:45:15 -0300

    Intro HTML e Damas

--- Damas/Damas/ProjetoDama.py
+++ Damas/Damas/ProjetoDama.py
@@ -3,19 +3,19 @@
 #------------------------ CRIA TABULEIRO ---------------------------
 def criaTabuleiro():
     """
-    cria uma matriz 8x8 e adiciona uma string vazia a cada posição
+    cria uma matriz 8x8 e adiciona uma string vazia a cada posiï¿½ï¿½o
     da matriz.
     """
     cTabuleiro = []
-    
+
     for i in range(8):
         elemento = []
         for j in range(8):
             elemento.append("")
-            
+
         cTabuleiro.append(elemento)
-    
-    return cTabuleiro     
+
+    return cTabuleiro
 
 tabuleiro = criaTabuleiro()
 
@@ -23,43 +23,43 @@ tabuleiro = criaTabuleiro()
 def verificaTamanhoTabuleiro(tabuleiro):
     """
     recebe uma matriz (8x8) e verifica se ela tem 8 linhas e 8 colunas,
-    se não retorna False se tem retorna True. 
+    se nï¿½o retorna False se tem retorna True.
     """
     for i in range(len(tabuleiro)):
         for j in range(len(tabuleiro[i])):
             if(len(tabuleiro) or len(tabuleiro[j]) != 8):
                 return False
-            
+
     return True
 
 #------------------- INICIALIZA TABULEIRO -------------------------
 def inicializaTabuleiro(tabuleiro):
     """
     Recebe uma matriz 8x8 que representa uma tabuleiro
-    Uma peça é representada por uma tupla que contém 2 caracteres, o primeiro indica a a cor da peça.
+    Uma peï¿½a ï¿½ representada por uma tupla que contï¿½m 2 caracteres, o primeiro indica a a cor da peï¿½a.
     """
     verificaTamanhoTabuleiro(tabuleiro)
-    
+
     for i in range(len(tabuleiro)):
         for j in range(len(tabuleiro)):
             if(i == 0) or (i == 1) or (i == 6) or (i == 7):
                 if(i == 0) or (i == 1):
                     cor = "w"
-                      
+
                 elif(i == 6) or (i == 7):
-                    cor = "b" 
-                      
+                    cor = "b"
+
             if (i==1):
                 if(j==1) or (j==3)or(j==5)or(j==7):
-                    peca="p" 
+                    peca="p"
                 elif(j==0) or (j==2)or (j==4)or (j==6):
-                    cor,peca=" "," "   
+                    cor,peca=" "," "
             if (i==6):
                 if(j==0) or (j==2)or(j==4)or(j==6):
-                    peca="p" 
+                    peca="p"
                 elif(j==1) or (j==3)or (j==5)or (j==7):
-                    cor,peca=" "," "          
-            
+                    cor,peca=" "," "
+
             if(i == 0):
                 if(j == 0):
                     peca = "p"
@@ -70,7 +70,7 @@ def inicializaTabuleiro(tabuleiro):
                 elif(j == 4):
                     peca = "p"
                 elif(j==1) or (j==3)or (j==5)or (j==7):
-                    cor,peca=" "," "   
+                    cor,peca=" "," "
             if(i == 7):
                 if(j == 1):
                     peca = "p"
@@ -81,63 +81,63 @@ def inicializaTabuleiro(tabuleiro):
                 elif(j == 7):
                     peca = "p"
                 elif(j==0) or (j==2)or (j==4)or (j==6):
-                    cor,peca=" "," "           
-                    
+                    cor,peca=" "," "
+
             if(i == 2) or (i == 3) or (i == 4) or (i == 5):
                 cor,peca = " "," "
 
             tabuleiro[i][j] = (cor,peca)
-                 
+
     return tabuleiro
- 
-#------------------------------- RETORNAPECA() -----------------------------------------------
+
+#------------------------------- RETORNAPECA() ---------------------------------
 tabuleiro = inicializaTabuleiro(tabuleiro)
 
 def retornaPeca(tabuleiro, posicao):
-    
+
     """
-    Recebe um tabuleiro (lista 8x8) e uma posicao (tupla com 2 números informando linha e coluna)
-    como argumento e retorna qual é a peça (uma tupla) que está naquela posição.
+    Recebe um tabuleiro (lista 8x8) e uma posicao (tupla com 2 nÃºmeros informando linha e coluna)
+    como argumento e retorna qual Ã© a peÃ§a (uma tupla) que estÃ¡ naquela posiÃ§Ã£o.
     """
     verificaTamanhoTabuleiro(tabuleiro)
-    
-    i, j = posicao 
-    
+
+    i, j = posicao
+
     if(0 <= (i or j) <= 7):
         peca = tabuleiro[i][j]
     else:
         return False
-    
-    return peca, i, j   
 
-#------------------------------------ MOVEPECA() -----------------------------------------------
+    return peca, i, j
+
+#------------------------------------ MOVEPECA() -------------------------------
 def movePeca(tabuleiro, posicaoIni, posicaoFim):
-    
+
     """
     Recebe um tabuleiro, uma posicao inicial e uma posicao final.
     Move a peca da posicao inicial para a posicao final.
-    Retorna True se for possível mover a peça e retorna False caso contrário.
+    Retorna True se for possï¿½vel mover a peï¿½a e retorna False caso contrï¿½rio.
     """
     verificaTamanhoTabuleiro(tabuleiro)
-    
-    
+
+
     pecaIni, i, j = retornaPeca(tabuleiro, posicaoIni)
     pecaFim, k, l = retornaPeca(tabuleiro, posicaoFim)
-     
+
     if((pecaIni and pecaFim) != False):
         if(pecaIni != ""):
             if(l != j)and((j==l-1 or j==l+1) and (i==k+1 or i== k-1)):
-                
+
               if(pecaIni[0] == "w") and (pecaFim[0] == "b" or pecaFim[0] == " "):
-                tabuleiro[k][l], tabuleiro[i][j] = pecaIni, (" "," ")          
+                tabuleiro[k][l], tabuleiro[i][j] = pecaIni, (" "," ")
               elif(pecaIni[0] == "b") and (pecaFim[0] == "w" or pecaFim[0] == " "):
                 tabuleiro[k][l], tabuleiro[i][j] = pecaIni, (" "," ")
-              #peca Dama. 
+              #peca Dama.
               elif(pecaFim[0]==" " and pecaIni[0]=="w")and(k==7):
-                tabuleiro[k][l], tabuleiro[i][j] = ("W","D"), (" "," ")    
+                tabuleiro[k][l], tabuleiro[i][j] = ("W","D"), (" "," ")
               elif(pecaFim[0]=="b" and pecaIni[0]=="b")and(k==0):
                 tabuleiro[k][l], tabuleiro[i][j] = ("B","D"), (" "," ")
-                                  
+
               else:
                 print "ERROR......."
             else:
@@ -146,26 +146,26 @@ def movePeca(tabuleiro, posicaoIni, posicaoFim):
             return False
     else:
         return False
-    
+
     return True
 
 
-#---------------------------------- IMPRIMETABULEIRO() --------------------------------------
+#---------------------------------- IMPRIMETABULEIRO() -------------------------
 def imprimeTabuleiro(tabuleiro):
-    
+
     """
-    Imprime na saída padrão um tabuleiro de maneira que um jogador de xadrez possa entendê-lo.
+    Imprime na saï¿½da padrï¿½o um tabuleiro de maneira que um jogador de xadrez possa entendï¿½-lo.
     """
-    
+
     print "="*51
     print "PROJETO DAMAS".center(51)
     print "="*51
-    
+
     for k in range(len(tabuleiro)):
         print "%5i"%k,
-    
-    print ""    
-    
+
+    print ""
+
     for i in range(len(tabuleiro)):
         print "-"*50
         print "%i"%i,
@@ -173,15 +173,15 @@ def imprimeTabuleiro(tabuleiro):
             print "|",
             print "%2s%s"%tabuleiro[i][j],
         print "|"
-    
-    print "-"*51
-    
 
-#-------------------------------FUNÇÃO CRIAR ARQUIVO-----------------------------------------
+    print "-"*51
+
+
+#-------------------------------FUNÃ‡ÃƒO CRIAR ARQUIVO--------------------------
 #def criaArquivo(peca,posicao):
-#    
+#
 #    arquivo = open('Jogadas.txt','r+')
-#    
+#
 #    if arquivo.readline() != None:
 #        arquivo.write("Jogada - ")
 #        arquivo.writelines(str(peca))
